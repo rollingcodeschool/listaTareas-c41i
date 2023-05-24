4bfc5dd236dd9deffeae0386f72062d3cd68e4d0
Emilse Arias <emilsearias@gmail.com>
2023-05-24 17:30:03 -0300

    actualizamos el valor de la tarea con lo que el usuario escribio en el input

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,15 +1,27 @@
 import { Button, Form } from "react-bootstrap";
 // import Form from 'react-bootstrap/Form';
 // import Button from 'react-bootstrap/Button';
-
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  }
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
